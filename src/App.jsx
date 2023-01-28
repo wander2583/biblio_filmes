7af3ae8ef9826a85af1e125b5dd105b2012a3c71
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-01-27 21:42:52 -0300

    feat: Importing icons and navbar to the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Link, Outlet } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
   );
 }
