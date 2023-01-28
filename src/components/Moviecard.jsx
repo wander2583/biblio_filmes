476441dbbe2cbd85429f7729f7994e0197c8b43f
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-01-28 08:28:45 -0300

    feat: creating the moviecard and configuring.

---
+++ src/components/Moviecard.jsx
@@ -0,0 +1,11 @@
+import { FaStar } from "react-icons/fa"
+
+const imageUrl = import.meta.env.VITE_IMG
+
+const Moviecard = () => {
+  return (
+    <div>Moviecard</div>
+  )
+}
+
+export default Moviecard
