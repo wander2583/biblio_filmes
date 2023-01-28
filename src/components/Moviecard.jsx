303bf3aa96a39fdfc0bb71899c0c90856572d3de
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-01-28 08:55:07 -0300

    feat: finalizing the moviecard

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,11 +1,17 @@
 import { FaStar } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const imageUrl = import.meta.env.VITE_IMG
 
-const Moviecard = () => {
-  return (
-    <div>Moviecard</div>
-  )
+const Moviecard = ({movie, showLink = true }) => {
+  return <div className="movie-card">
+    <img src={imageUrl + movie.poster_path} alt={movie.title} /> 
+    <h2>{movie.title}</h2>
+    <p>
+      <FaStar /> {movie.vote_average}
+    </p>
+    {showLink && <Link to={`/movie/${movie.id}`}></Link>}
+  </div> 
 }
 
 export default Moviecard
