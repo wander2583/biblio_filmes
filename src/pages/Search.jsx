afeb279c8f2018263e5438e4c3d8fe8027516658
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-01-28 12:55:31 -0300

    feat: building the project's search page.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import Moviecard from "../components/Moviecard"
+
+import './MoviesGrid.css'
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Search = () => {
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+  const query = searchParams.get("q")
+
+  const getSearchMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setMovies(data.results)
+  }
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+
+    getSearchMovies(searchWithQueryURL)
+  }, [query]);
+
+
   return (
-    <div>Search</div>
+    <div className="container">
+      <h2 className="title">Resultados para: <span className="query-text">{query}</span> </h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p>carregando...</p>}
+        {movies.length > 0 && movies.map((movie) => <Moviecard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   )
 }
 
